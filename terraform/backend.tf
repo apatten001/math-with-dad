22c872a9bad965af7bd16773abe1836776c06101
Arnold Patten <apatten001@yahoo.com>
2025-11-23 13:20:28 -0500

    format tf files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "math-with-dad-terraform-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-2"
-    
+    bucket = "math-with-dad-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "ecr_repository_url" {
   description = "ECR repository URL with tag for the Docker image"
   type        = string
-  default     = null   # allows it to be passed via -var
+  default     = null # allows it to be passed via -var
 }
