71b383612e784bf2c4dfeef9694ffa0478f9d957
Arnold Patten <apatten001@yahoo.com>
2025-11-23 13:01:03 -0500

    format main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,9 +51,9 @@ resource "aws_cloudfront_distribution" "cdn" {
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3Origin"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3Origin"
     viewer_protocol_policy = "redirect-to-https"
     forwarded_values {
       query_string = false
@@ -65,12 +65,15 @@ resource "aws_cloudfront_distribution" "cdn" {
     compress    = true
   }
 
-  restrictions { 
-    geo_restriction { 
-        restriction_type = "none" }
-         }
-  viewer_certificate { 
-     = true }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
+  }
 }
 
 output "website_url" {
