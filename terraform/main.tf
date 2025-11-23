b7b8456a95cde6e9b73d8a2cc4fec10b5a463165
Arnold Patten <apatten001@yahoo.com>
2025-11-22 19:12:26 -0500

    Make changes for Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,11 +65,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     compress    = true
   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+  restrictions { geo_restriction { restriction_type = "none" } }
   viewer_certificate { cloudfront_default_certificate = true }
 }
 
